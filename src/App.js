075488d44dba39582d347441e264374e3dd0632d
sahilhedau49 <hedausahil49@gmail.com>
2023-08-26 16:40:55 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
+import Bounce from "react-reveal/Bounce";
 
 function App() {
   const [data, setData] = useState({ name: "", phone: "", emailid: "" });
@@ -18,38 +20,72 @@ function App() {
     setSuccess(true);
     setTimeout(() => {
       setSuccess(false);
-    }, 2000);
-    console.log(data);
+    }, 2500);
+    // console.log(data);
     try {
       await axios.post("http://localhost:5000/", data);
-      console.log("DATA ADDED");
+      // console.log("DATA ADDED");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="App">
-      <form>
-        <label for="name">Name: </label>
-        <input type="text" name="name" onChange={handleChange} />
+    <div className="App w-screen min-h-screen bg-slate-300 p-20">
+      <h2 className="text-center font-bold text-3xl text-gray-900 mb-2">
+        Contact Us
+      </h2>
+      <form className="form w-[30%] mx-auto p-10 rounded-xl">
+        <label for="name" className="text-2xl">
+          Name:
+        </label>
         <br />
-        <label for="phone">Phone: </label>
-        <input type="number" name="phone" onChange={handleChange} />
+        <input
+          className="w-[100%] mb-2 text-lg outline-none border-[2px] rounded-xl px-4 py-1 focus:border-black"
+          type="text"
+          name="name"
+          onChange={handleChange}
+        />
         <br />
-        <label for="email">Email: </label>
-        <input type="email" name="emailid" onChange={handleChange} />
+        <label for="phone" className="text-2xl">
+          Phone:{" "}
+        </label>
         <br />
-        <button type="submit" onClick={handleSubmit}>
+        <input
+          className="w-[100%] mb-2 text-lg outline-none border-[2px] rounded-xl px-4 py-1 focus:border-black"
+          type="number"
+          name="phone"
+          onChange={handleChange}
+        />
+        <br />
+        <label for="email" className="text-2xl">
+          Email:{" "}
+        </label>
+        <br />
+        <input
+          className="w-[100%] mb-2 text-lg outline-none border-[2px] rounded-xl px-4 py-1 focus:border-black"
+          type="email"
+          name="emailid"
+          onChange={handleChange}
+        />
+        <br />
+        <button
+          className="block btn mx-auto my-4 text-xl border-2 px-8 py-1 rounded-full"
+          type="submit"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </form>
+
       {success && (
-        <div>
-          <div className="w-full text-center bg-gray-950 text-slate-200">
-            Success
+        <Bounce left>
+          <div>
+            <div className="block mx-auto my-2 px-3 py-1 w-40 text-2xl text-center bg-green-600 text-slate-200 rounded-3xl">
+              Success ✅
+            </div>
           </div>
-        </div>
+        </Bounce>
       )}
     </div>
   );
